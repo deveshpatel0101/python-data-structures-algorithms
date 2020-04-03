f0c6ebeca9ee30d34aecea5ae45200c875be11ff
computer0101 <deveshpatel7643@gmail.com>
2020-04-03 19:14:15 +0530

    implement simple binary tree using array

---
+++ trees/simple_bt_using_arr/traversal.py
@@ -0,0 +1,194 @@
+class ArrayTreeTraversal:
+    def __init__(self, data):
+        self.tree = data
+        self.size = len(data)
+
+    def __getChildrenIndices(self, index):
+        leftChildIndex = (index*2)+1
+        rightChildIndex = (index*2)+2
+
+        if not (leftChildIndex < len(self.tree) and self.tree[leftChildIndex]):
+            leftChildIndex = None
+        if not (rightChildIndex < len(self.tree) and self.tree[rightChildIndex]):
+            rightChildIndex = None
+
+        return (leftChildIndex, rightChildIndex)
+
+    # bfsTraversal is a recursive version of bfs traversal
+    def bfsTraversal(self):
+        root = 0
+        queue = []
+        path = []
+
+        if self.tree[root] == None:
+            return []
+
+        queue.append(root)
+        path = self.bfsTraversalUtil(queue, path)
+        return path
+
+    # bfsTraversalUtil recursively calls itself to find a bfs traversal path
+    def bfsTraversalUtil(self, queue, path):
+        if len(queue) == 0:
+            return []
+
+        currIndex = queue[0]
+        path.append(self.tree[currIndex])
+        queue = queue[1:]
+        childrenIndices = self.__getChildrenIndices(currIndex)
+        queue.extend(
+            [childIndex for childIndex in childrenIndices if childIndex])
+
+        self.bfsTraversalUtil(queue, path)
+
+        return path
+
+    # dfsTraversal calls appropriate recursive pre, in or post order traversals
+    def dfsTraversal(self, order):
+        root = 0
+        stack = []
+        path = []
+        if self.tree[root] == None:
+            return []
+
+        stack.append(root)
+
+        if order == 'pre':
+            path = self.traversePre(path, 0)
+        elif order == 'in':
+            path = self.traverseIn(path, 0)
+        elif order == 'post':
+            path = self.traversePost(path, 0)
+
+        return path
+
+    # parent left right
+    # TraversePre is a recursive version of dfs pre-order traversal
+    def traversePre(self, path, index):
+        if len(self.tree) > index and self.tree[index]:
+            path.append(self.tree[index])
+            path = self.traversePre(path, (index*2)+1)
+            path = self.traversePre(path, (index*2)+2)
+
+        return path
+
+    # left parent right
+    # TraverseIn is a recursive version of dfs in-order traversal
+    def traverseIn(self, path, index):
+        if len(self.tree) > index and self.tree[index]:
+            path = self.traverseIn(path, (index*2)+1)
+            path.append(self.tree[index])
+            path = self.traverseIn(path, (index*2)+2)
+
+        return path
+
+    # left right parent
+    # TraversePost is a recursive version of dfs post-order traversal
+    def traversePost(self, path, index):
+        if len(self.tree) > index and self.tree[index]:
+            path = self.traversePost(path, (index*2)+1)
+            path = self.traversePost(path, (index*2)+2)
+            path.append(self.tree[index])
+
+        return path
+
+    # iteratvieBfsTraversal is an itervative version of bfs traversal
+    def iterativeBfsTraversal(self):
+        root = 0
+        queue = []
+        path = []
+        if self.tree[root] == None:
+            return []
+
+        queue.append(root)
+        while len(queue) != 0:
+            currIndex = queue[0]
+            path.append(self.tree[currIndex])
+            queue = queue[1:]
+            childrenIndices = self.__getChildrenIndices(currIndex)
+            queue.extend(
+                [childIndex for childIndex in childrenIndices if childIndex])
+
+        return path
+
+    # parent left right
+    # itervativeDfsPreOrderTraversal is iterative version of dfs pre-order traversal
+    def iterativeDfsPreOrderTraversal(self):
+        stack = []
+        root = 0
+        path = []
+
+        if self.tree[root] == None:
+            return []
+
+        stack.append(root)
+        while len(stack) != 0:
+            parentIndex = stack[len(stack)-1]
+            path.append(self.tree[parentIndex])
+            stack = stack[:len(stack)-1]
+            childrenIndices = self.__getChildrenIndices(parentIndex)
+            childrenIndices = childrenIndices[::-1]
+
+            stack.extend(
+                [childIndex for childIndex in childrenIndices if childIndex])
+
+        return path
+
+    # left parent right
+    # iterativeDfsInOrderTraversal is iterative version of dfs in-order traversal
+    def iterativeDfsInOrderTraversal(self):
+        stack = []
+        path = []
+        currIndex = 0
+        while True:
+            while currIndex != None:
+                stack.append(currIndex)
+                currIndex = self.__getChildrenIndices(currIndex)[0]
+
+            if currIndex == None and len(stack) != 0:
+                item = stack[len(stack)-1]
+                stack = stack[:len(stack)-1]
+
+                path.append(self.tree[item])
+                currIndex = self.__getChildrenIndices(item)[1]
+            if currIndex == None and len(stack) == 0:
+                break
+
+        return path
+
+    # left right parent
+    # iterativeDfsPostOrderTraversal is iterative version of dfs post-order traversal
+    def iterativeDfsPostOrderTraversal(self):
+        stack = []
+        path = []
+        currIndex = 0
+
+        while True:
+            while currIndex != None:
+                (leftChildIndex, rightChildIndex) = self.__getChildrenIndices(currIndex)
+                if rightChildIndex:
+                    stack.append(rightChildIndex)
+                stack.append(currIndex)
+                currIndex = leftChildIndex
+
+            currIndex = stack[len(stack)-1]
+            stack = stack[:len(stack)-1]
+            (leftChildIndex, rightChildIndex) = self.__getChildrenIndices(currIndex)
+
+            if rightChildIndex and len(stack) != 0 and stack[len(stack)-1] == rightChildIndex:
+                stack = stack[:len(stack)-1]
+                stack.append(currIndex)
+                currIndex = rightChildIndex
+            else:
+                path.append(self.tree[currIndex])
+                currIndex = None
+
+            if len(stack) == 0:
+                break
+
+        return path
+
+
+# References:
+# Algorithms for iterative in-order traversal: https://www.geeksforgeeks.org/inorder-tree-traversal-without-recursion/
+# Algorithm for iterative post-order traversal: https://www.geeksforgeeks.org/iterative-postorder-traversal-using-stack/
---
+++ trees/simple_bt_using_arr/tree.py
@@ -0,0 +1,111 @@
+from traversal import ArrayTreeTraversal
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.freq = 1
+
+
+class SimpleBinaryTree:
+    def __init__(self):
+        self.tree = []
+
+    def __getChildren(self, index):
+        leftChildIndex = (index*2)+1
+        rightChildIndex = (index*2)+2
+        leftChild = None
+        rightChild = None
+
+        if leftChildIndex < len(self.tree):
+            leftChild = self.tree[leftChildIndex]
+        if rightChildIndex < len(self.tree):
+            rightChild = self.tree[rightChildIndex]
+
+        return (leftChild, rightChild)
+
+    def __getSibling(self, index, siblingOf):
+        parentIndex = -1
+        if siblingOf == 'left':
+            parentIndex = int((index-1)/2)
+        else:
+            parentIndex = int((index-2)/2)
+
+        if siblingOf == 'left' and (parentIndex*2)+2 < len(self.tree):
+            return self.tree[(parentIndex*2)+2]
+        elif siblingOf == 'right' and (parentIndex*2)+1 < len(self.tree):
+            return self.tree[(parentIndex*2)+1]
+
+        return None
+
+    def __getNodeInfo(self, index):
+        data = {'found': None, 'freq': None, 'parent': None, 'sibling': None,
+                'leftChild': None, 'rightChild': None}
+        if index == 0:
+            # fount root element
+            data['found'] = self.tree[index].data
+            data['freq'] = self.tree[index].freq
+            (leftChild, rightChild) = self.__getChildren(index)
+            data['leftChild'] = leftChild.data if leftChild else None
+            data['rightChild'] = rightChild.data if rightChild else None
+            return data
+
+        if index % 2 != 0:
+            # found left element
+            parentIndex = int((index-1)/2)
+            (leftChild, rightChild) = self.__getChildren(index)
+            sibling = self.__getSibling(index, 'left')
+            data['found'] = self.tree[index].data
+            data['freq'] = self.tree[index].freq
+            data['parent'] = self.tree[parentIndex].data
+            data['sibling'] = sibling.data if sibling else None
+            data['leftChild'] = leftChild.data if leftChild else None
+            data['rightChild'] = rightChild.data if rightChild else None
+        else:
+            # found right element
+            parentIndex = int((index-2)/2)
+            (leftChild, rightChild) = self.__getChildren(index)
+            sibling = self.__getSibling(index, 'right')
+            data['found'] = self.tree[index].data
+            data['freq'] = self.tree[index].freq
+            data['parent'] = self.tree[parentIndex].data
+            data['sibling'] = sibling.data if sibling else None
+            data['leftChild'] = leftChild.data if leftChild else None
+            data['rightChild'] = rightChild.data if rightChild else None
+
+        return data
+
+    def insert(self, data):
+        for node in self.tree:
+            if node.data == data:
+                node.freq += 1
+                return
+
+        self.tree.append(Node(data))
+
+    def search(self, data):
+        for index in range(len(self.tree)):
+            if self.tree[index].data == data:
+                return self.__getNodeInfo(index)
+
+        raise Exception('\n-- Data not found --')
+
+    def simpleDisplay(self):
+        for index in range(len(self.tree)):
+            print(
+                f'{index}. Data: {self.tree[index].data}, Freq: {self.tree[index].freq}')
+
+    def getTree(self):
+        return self.tree
+
+    def bfsTraversal(self):
+        return ArrayTreeTraversal(self.tree).bfsTraversal()
+
+    def dfsPreOrderTraversal(self):
+        return ArrayTreeTraversal(self.tree).dfsTraversal('pre')
+
+    def dfsInOrderTraversal(self):
+        return ArrayTreeTraversal(self.tree).dfsTraversal('in')
+
+    def dfsPostOrderTraversal(self):
+        return ArrayTreeTraversal(self.tree).dfsTraversal('post')
