d9f972673225830f9251649c0f1b4f7d1304788a
computer0101 <deveshpatel7643@gmail.com>
2020-10-10 23:39:18 +0530

    add docstrings to stack

--- stack/__main__.py
+++ stack/__main__.py
@@ -1,4 +1,4 @@
-from stack import Stack
+from .stack import Stack
 
 
 def start():
--- stack/stack.py
+++ stack/stack.py
@@ -1,16 +1,29 @@
 class Stack:
-    def __init__(self, size = 5):
+    '''
+    Stack is a data structure that implements LIFO/FILO
+    '''
+
+    def __init__(self, size=5):
         self.items = []
         self.size = size
         self.top = 0
 
     def set_size(self, size):
+        '''
+        sets the maximum allowed size of stack
+        '''
         self.size = size
 
     def get_size(self):
+        '''
+        returns the size of stack
+        '''
         return self.size
 
     def push(self, item):
+        '''
+        pushes an element at the top of stack
+        '''
         if self.top == self.size:
             raise Exception('\n-- Stack Overflow! --')
         self.items.append(item)
@@ -18,18 +31,30 @@ class Stack:
         return item, self.top
 
     def pop(self):
+        '''
+        pops an element from the top of stack and return it
+        '''
         if self.top == 0:
             raise Exception('\n-- Stack underflow! --')
         self.top -= 1
         return self.items.pop(), self.top
 
     def is_empty(self):
+        '''
+        returns True if stack is empty and False otherwise
+        '''
         return self.items == []
 
     def peek(self):
+        '''
+        returns the element that is on the top of stack
+        '''
         if self.top == 0:
             raise Exception('\n-- Stack underflow! --')
         return self.items[len(self.items)-1]
 
     def display(self):
+        '''
+        returns all the elements present in the stack
+        '''
         return self.items
