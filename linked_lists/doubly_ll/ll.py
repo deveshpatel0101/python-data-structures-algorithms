93c038acc3e21eafd37a2c728aa49a2150c72955
computer0101 <deveshpatel7643@gmail.com>
2020-10-25 22:53:12 +0530

    add tests for doubly ll

--- linked_lists/doubly_ll/ll.py
+++ linked_lists/doubly_ll/ll.py
@@ -1,8 +1,15 @@
 class Node:
+    '''
+    A node data structure holds two values:
+    1. Data to store in the node
+    2. A pointer to the next node
+    3. A pointer to the previous node
+    '''
+
     def __init__(self, data):
         self.data = data
-        self.prev = None
-        self.next = None
+        self.prev: Node = None
+        self.next: Node = None
 
 
 class DoublyLinkedList:
@@ -12,6 +19,10 @@ class DoublyLinkedList:
 
     # insert at start
     def unshift(self, data):
+        '''
+        Inserts a node at the start
+        '''
+
         node = Node(data)
         if self.head == None:
             self.head = self.tail = node
@@ -21,32 +32,42 @@ class DoublyLinkedList:
             self.head = node
 
     # insert at middle
-    def insertMiddle(self, num, data):
-        if self.head == None and num > 1:
-            raise Exception('\n-- Index out of range --')
-        elif self.head == None or num == 1:
-            self.unshift(data)
-        else:
-            node = Node(data)
-            currNode = self.head
+    def insertMiddle(self, position, data):
+        '''
+        Inserts a node at a given position
+        '''
 
-            for i in range(0, num-2):
-                if currNode == self.tail:
-                    raise Exception('\n-- Index out of range --')
-                currNode = currNode.next
+        if position <= 0:
+            raise Exception
+        elif position == 1:
+            return self.unshift(data)
+        elif self.head == None:
+            raise Exception
 
+        node = Node(data)
+        currNode = self.head
+
+        for _ in range(0, position-2):
             if currNode == self.tail:
-                node.prev = self.tail
-                self.tail.next = node
-                self.tail = node
-            else:
-                node.prev = currNode
-                node.next = currNode.next
-                currNode.next = node
-                node.next.prev = node
+                raise Exception
+            currNode = currNode.next
+
+        if currNode == self.tail:
+            node.prev = self.tail
+            self.tail.next = node
+            self.tail = node
+        else:
+            node.prev = currNode
+            node.next = currNode.next
+            currNode.next = node
+            node.next.prev = node
 
     # insert at end
     def push(self, data):
+        '''
+        Inserts a node at the end
+        '''
+
         node = Node(data)
         if self.head == None:
             self.head = self.tail = node
@@ -55,66 +76,79 @@ class DoublyLinkedList:
             self.tail.next = node
             self.tail = node
 
-    # delete from start
+    # remove from start
     def shift(self):
-        if self.head == None or self.tail == None:
-            raise Exception('\n-- List is empty --')
-
-        node = self.head
-        self.head = self.head.next
+        '''
+        removes the first node and returns the data stored in it
+        '''
 
         if self.head == None:
-            self.tail = None
-        else:
-            self.head.prev = None
+            raise Exception
 
-        return node.data
-
-    # delete from middle
-    def deleteMiddle(self, num):
-        if self.head == None and num > 1:
-            raise Exception('\n-- Index out of range --')
-        elif self.head == None or num == 1:
-            return self.shift()
-        else:
-            currNode = self.head
-
-            for i in range(0, num-2):
-                if currNode.next == self.tail:
-                    raise Exception('\n-- Index out of range --')
-                currNode = currNode.next
-            data = None
-            data = currNode.next.data
-            if currNode.next == self.tail:
-                currNode.next = None
-                self.tail = currNode
-            else:
-                currNode.next = currNode.next.next
-                currNode.next.prev = currNode
-
-            return data
-
-    # delete from end
-    def pop(self):
-        if self.head == None or self.tail == None:
-            raise Exception('\n-- List is empty --')
-
-        node = self.head
+        data = self.head.data
         if self.head == self.tail:
             self.head = self.tail = None
-            return node.data
+        else:
+            self.head = self.head.next
+            self.head.prev = None
 
-        while node.next.next != None:
-            node = node.next
+        return data
 
-        data = node.next.data
-        self.tail = node
+    # remove from middle
+    def removeMiddle(self, position):
+        '''
+        removes a node from the specified position and returns the data stored in it
+        '''
+
+        if position <= 0:
+            raise Exception
+        elif position == 1:
+            return self.shift()
+        elif self.head == None:
+            raise Exception
+
+        currNode = self.head
+
+        for _ in range(0, position-2):
+            if currNode.next == self.tail:
+                raise Exception
+            currNode = currNode.next
+
+        data = currNode.next.data
+        if currNode.next == self.tail:
+            currNode.next = None
+            self.tail = currNode
+        else:
+            currNode.next = currNode.next.next
+            currNode.next.prev = currNode
+
+        return data
+
+    # remove from end
+    def pop(self):
+        '''
+        removes a node from the end and returns the data stored in it
+        '''
+
+        if self.head == None:
+            raise Exception
+        elif self.head == self.tail:
+            data = self.head.data
+            self.head = self.tail = None
+            return data
+
+        data = self.tail.data
+        self.tail = self.tail.prev
         self.tail.next = None
         return data
 
     def reverse(self):
+        '''
+        reverses the linked list
+        '''
+
         if self.head == None:
-            raise Exception('\n-- List is empty --')
+            raise Exception
 
         node = self.head
         while node != None:
@@ -127,5 +161,9 @@ class DoublyLinkedList:
         self.head = self.tail
         self.tail = temp
 
-    def display(self):
+    def getPointers(self):
+        '''
+        returns the head and tail pointers
+        '''
+
         return self.head, self.tail
---
+++ linked_lists/doubly_ll/test_ll.py
@@ -0,0 +1,190 @@
+import unittest
+from random import randint
+
+from linked_lists.doubly_ll.ll import DoublyLinkedList
+from linked_lists.doubly_ll.ll import Node
+
+
+class TestDoublyLinkedList(unittest.TestCase):
+    def verify_none(self, ll: DoublyLinkedList):
+        head, tail = ll.getPointers()
+
+        self.assertIsNone(head)
+        self.assertIsNone(tail)
+
+        self.assertRaises(Exception, ll.pop)
+        self.assertRaises(Exception, ll.removeMiddle, randint(1, 50))
+        self.assertRaises(Exception, ll.insertMiddle, 0, randint(1, 50))
+        self.assertRaises(Exception, ll.shift)
+
+    def test_insert_head(self):
+        ll = DoublyLinkedList()
+
+        self.verify_none(ll)
+
+        pushedElements = []
+        element = randint(1, 50)
+        pushedElements.append(element)
+        ll.unshift(element)
+
+        head, tail = ll.getPointers()
+        self.assertEqual(head, tail)
+        self.verify_data(pushedElements, ll.getPointers()[0])
+
+        for _ in range(randint(5, 20)):
+            element = randint(1, 50)
+            pushedElements.append(element)
+            ll.unshift(element)
+            self.verify_data(pushedElements[::-1], ll.getPointers()[0])
+
+    def test_insert_middle(self):
+        ll = DoublyLinkedList()
+
+        self.verify_none(ll)
+
+        # number of elements to add from head and tail
+        testPushHead = 5
+        testPushMiddle = 10
+        testPushTail = 5
+
+        pushedElements = []
+        element = randint(1, 50)
+        pushedElements.append(element)
+        ll.insertMiddle(1, element)
+
+        head, tail = ll.getPointers()
+        self.assertEqual(head, tail)
+
+        for _ in range(testPushHead):
+            element = randint(1, 50)
+            pushedElements = [element] + pushedElements
+            ll.insertMiddle(1, element)
+            self.verify_data(pushedElements, ll.getPointers()[0])
+
+        for _ in range(1, testPushTail):
+            element = randint(1, 50)
+            pushedElements.append(element)
+            ll.insertMiddle(len(pushedElements), element)
+            self.verify_data(pushedElements, ll.getPointers()[0])
+
+        for _ in range(testPushMiddle):
+            element = randint(1, 50)
+            position = randint(1, len(pushedElements)-1)
+            ll.insertMiddle(position, element)
+            pushedElements = pushedElements[:position-1] + \
+                [element] + pushedElements[position-1:]
+            self.verify_data(pushedElements, ll.getPointers()[0])
+
+    def test_insert_tail(self):
+        ll = DoublyLinkedList()
+
+        self.verify_none(ll)
+
+        pushedElements = []
+        element = randint(1, 50)
+        pushedElements.append(element)
+        ll.push(element)
+        head, tail = ll.getPointers()
+        self.assertEqual(head, tail)
+        self.verify_data(pushedElements, head)
+
+        for _ in range(10):
+            element = randint(1, 50)
+            pushedElements.append(element)
+            ll.push(element)
+            self.verify_data(pushedElements, ll.getPointers()[0])
+
+    def test_remove_head(self):
+        ll = DoublyLinkedList()
+
+        self.verify_none(ll)
+
+        pushedElements = self.fill_data(ll)
+
+        for _ in range(10):
+            element = pushedElements[0]
+            pushedElements = pushedElements[1:]
+            removedElement = ll.shift()
+            self.assertEqual(removedElement, element)
+            self.verify_data(pushedElements, ll.getPointers()[0])
+
+    def test_remove_middle(self):
+        ll = DoublyLinkedList()
+
+        self.verify_none(ll)
+
+        self.assertRaises(Exception, ll.removeMiddle, 0)
+
+        # number of elements to add from head and tail
+        testRemoveHead = 5
+        testRemoveMiddle = 10
+        testRemoveTail = 5
+
+        pushedElements = self.fill_data(ll)
+        self.verify_data(pushedElements, ll.getPointers()[0])
+
+        for _ in range(testRemoveHead):
+            element = pushedElements[0]
+            pushedElements = pushedElements[1:]
+            removedElement = ll.removeMiddle(1)
+            self.assertEqual(removedElement, element)
+            self.verify_data(pushedElements, ll.getPointers()[0])
+
+        for _ in range(1, testRemoveTail):
+            element = pushedElements[-1]
+            pushedElements = pushedElements[:-1]
+            removedElement = ll.removeMiddle(len(pushedElements)+1)
+            self.assertEqual(element, removedElement)
+            self.verify_data(pushedElements, ll.getPointers()[0])
+
+        for _ in range(testRemoveMiddle):
+            position = randint(2, len(pushedElements)-1)
+            element = pushedElements[position-1]
+            pushedElements = pushedElements[:position -
+                                            1] + pushedElements[position:]
+            removedElement = ll.removeMiddle(position)
+            self.assertEqual(removedElement, element)
+            self.verify_data(pushedElements, ll.getPointers()[0])
+
+    def test_remove_tail(self):
+        ll = DoublyLinkedList()
+
+        self.verify_none(ll)
+
+        pushedElements = self.fill_data(ll)
+
+        for _ in range(10):
+            element = pushedElements[-1]
+            pushedElements = pushedElements[:-1]
+            removedElement = ll.pop()
+            self.assertEqual(removedElement, element)
+            self.verify_data(pushedElements, ll.getPointers()[0])
+
+    def test_reverse(self):
+        ll = DoublyLinkedList()
+
+        self.verify_none(ll)
+
+        pushedElements = self.fill_data(ll)
+
+        ll.reverse()
+        self.verify_data(pushedElements[::-1], ll.getPointers()[0])
+
+    def fill_data(self, ll: DoublyLinkedList, fill_size=25):
+        pushedElements = []
+
+        for _ in range(fill_size):
+            element = randint(1, 50)
+            pushedElements.append(element)
+            ll.push(element)
+
+        return pushedElements
+
+    def verify_data(self, items, head: Node):
+        currNode = head
+        arr = []
+        while currNode is not None:
+            arr.append(currNode.data)
+            currNode = currNode.next
+
+        self.assertEqual(items, arr)
